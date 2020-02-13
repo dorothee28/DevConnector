36013f5bfb41ab399452cc68fe4e622027341a28
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-13 20:10:42 +0100

    Request & body validation/ user post

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectBD();
 
+// Init Middleware shoul allow us to get data in body (req.body)
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running!'));
 
 // Define Routes
