a9101769006bbedcee9577969a78e4453be213bf
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-14 03:30:07 +0100

    Get All profiles & Profile by user ID

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -95,18 +95,51 @@ try {
 
         return res.json(profile);
     }
-    // Create
-    profile = new Profile(profileFields);
+        // Create
+        profile = new Profile(profileFields);
 
-    await profile.save();
-    res.json(profile);
+        await profile.save();
+        res.json(profile);
 
-} catch(err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-}
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
 
     }
 );
+
+// @route  GET api/Profile
+// @desc  Get all profiles
+// @access  Public
+router.get('/' , async (req, res) => {
+    try {
+        const profiles =  await Profile.find().populate('user' , ['name', 'avatar']);
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');   
+    }
+});
+
+// @route  GET api/Profile/user/:user_id
+// @desc  Get profile by user ID
+// @access  Public
+router.get('/user/:user_id' , async (req, res) => {
+    try {
+        const profiles =  await Profile.findOne({ user: req.params.user_id}).populate('user' ,
+         ['name', 'avatar']);
+
+         if(!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profiles);
+    } catch (err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');   
+    }
+});
 
 module.exports = router;
