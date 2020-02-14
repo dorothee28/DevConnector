649d215f028e94dd5cdb0e2dccfbc61ca9292c56
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-14 21:02:53 +0100

    Delele Profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -219,5 +219,28 @@ router.put('/experience' ,
   
 });
 
+// @route  DELETE api/Profile/experience/:exp_id
+// @desc DElete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+    // console.log(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');  
+    }
+
+})
 
 module.exports = router;
