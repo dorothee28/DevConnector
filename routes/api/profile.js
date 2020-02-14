76776057317e1659bf852a49d84b358cfe507295
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-14 01:04:09 +0100

    Get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
 
-// @route   GET api/Profile
-// @desc    Test route
-// @access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+// local import
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile')
+const User= require('../../models/User')
+
+// @route   GET api/Profile/me
+// @desc    get current users profile
+// @access  Private
+router.get('/me', auth, async(req, res) => {
+
+    try {
+        const profile = await (await Profile.findOne({ user: req.user.id })).populated(
+            'user',
+            ['name', 'avatar']
+        );
+
+        if(!profile) {
+            return res.status(400).json({ msg: 'There is no profile for thsi user' })
+        }
+
+        res.json(profile);
+        
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+
+});
 
 module.exports = router;
