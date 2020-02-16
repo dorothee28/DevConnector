eec5dd95bf27c962637a68035c5e829ac1d54de7
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-17 00:47:30 +0100

    Load User & Set Auth Token

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import  { Fragment } from 'react';
 
@@ -13,10 +14,21 @@ import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import  setAuthToken  from './utils/setAuthToken';
 
 import './App.css';
 
+if (localStorage.token)  {
+  setAuthToken(localStorage.token);
+}
+
+
 const App = ()  => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
     <Provider store={store}>
       <Router>
