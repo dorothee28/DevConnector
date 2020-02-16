f61a57f6661a4cb26efa9ecb7378ac4b2926c279
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-16 19:07:15 +0100

    Creating a Redux Stor

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import  { Fragment } from 'react'
+import  { Fragment } from 'react';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
 
 //local import 
 import Landing from './components/layout/Landing';
@@ -12,19 +17,20 @@ import './App.css';
 
 const App = ()  => {
   return (
-    <Router>
-        <Fragment>
-          <Route exact path='/' component={Landing} />
-          <section className="container">
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-            </Switch>      
-          </section>
-          <Navbar />
-        </Fragment>    
-    </Router>
-    
+    <Provider store={store}>
+      <Router>
+          <Fragment>
+            <Route exact path='/' component={Landing} />
+            <section className="container">
+              <Switch>
+                <Route exact path='/register' component={Register} />
+                <Route exact path='/login' component={Login} />
+              </Switch>      
+            </section>
+            <Navbar />
+          </Fragment>    
+      </Router>
+    </Provider> 
   );
 }
 
