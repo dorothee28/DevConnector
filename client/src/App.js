c124c2d3e21882c2db4b2b9b1ffe56a579f44416
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-16 02:30:59 +0100

    Login & register components & Router,Link setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import  { Fragment } from 'react'
 
 //local import 
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import './App.css';
 
 const App = ()  => {
   return (
-    <Fragment>
-      <Landing />
-      <Navbar />
-
-    </Fragment>
+    <Router>
+        <Fragment>
+          <Route exact path='/' component={Landing} />
+          <section className="container">
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>      
+          </section>
+          <Navbar />
+        </Fragment>    
+    </Router>
+    
   );
 }
 
