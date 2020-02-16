2e4170516352b0576919fc2bcc0b375c0588b73f
dorothebelle@hotmail.fr <dorothebelle@hotmail.fr>
2020-02-16 23:23:41 +0100

    Auth Reducer & Register Action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import { connect } from 'react-redux';
 import { Fragment, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { setAlert } from '../../actions/alert';
+import { register} from '../../actions/auth';
 import PropTypes from 'prop-types';
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert , register }) => {
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -25,7 +26,7 @@ const Register = ({ setAlert }) => {
            setAlert('Password do not match', 'danger')
         } else {
             // console.log(formData);
-           console.log('SUCCESS');
+        register({ name, email, password });
         }
     };
 
@@ -42,7 +43,7 @@ const Register = ({ setAlert }) => {
                      name="name"
                      value={name}
                      onChange={e => onChange(e)}
-                      required 
+                  
                       />
                 </div>
                 <div className="form-group">
@@ -52,7 +53,7 @@ const Register = ({ setAlert }) => {
                  name="email"
                  value={email}
                  onChange={e => onChange(e)}
-                 required
+               
                   />
                 <small className="form-text">This site uses Gravatar so if you want a profile image,
                  use a Gravatar email</small>
@@ -62,7 +63,6 @@ const Register = ({ setAlert }) => {
                         type="password"
                         placeholder="Password"
                         name="password"
-                        minLength="6"
                         value={password}
                         onChange={e => onChange(e)}
                     />
@@ -72,7 +72,6 @@ const Register = ({ setAlert }) => {
                         type="password"
                         placeholder="Confirm Password"
                         name="password2"
-                        minLength="6"
                         value={password2}
                         onChange={e => onChange(e)}
                     />
@@ -88,10 +87,11 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
     setAlert: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 };
 
 
 export default connect(
     null, 
-    { setAlert }
+    { setAlert , register}
 )(Register);
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,10 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
+import auth fom './auth';
+
 
 export default combineReducers({
     alert,
+    auth,
 
 });
